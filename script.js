2a3d62537e36e5df5564629546b12e427219d1f5
jiggy2204 <jignacio2204@gmail.com>
2020-10-24 20:26:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  window.onscroll = function () {
+    stickNavBar();
+  };
+
+  var navbar = $("#header-nav");
+
+  var sticky = navbar.offset();
+
+  function stickNavBar() {
+    if (window.pageYOffset >= sticky.top) {
+      navbar.addClass("sticky");
+    } else {
+      navbar.removeClass("sticky");
+    }
+  }
+
+  stickNavBar();
+});
